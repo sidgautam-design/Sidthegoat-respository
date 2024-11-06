4ee740dd95ce4e4a2426f9d734b5cc6a676538f9
sidgautam-design <siddhartha.gautam889@gmail.com>
2024-11-06 18:11:43 -0500

    Uploading my current working algorithm and flowchart

---
+++ python_1/sid.py
@@ -0,0 +1,21 @@
+# Snake Case
+firstName = "Siddhartha" # This is a string 
+
+# Camel Case 
+lastName = "Gautam" # This is also a string 
+
+
+age = 578 #This is an int 
+bankAccountBalance = 456.34 # This is a float
+
+# Command foward slash makes a comment
+print( firstName )
+print( bankAccountBalance )
+if bankAccountBalance > 100:
+     print( "You have so much moolah!" ) # this is also another type of string
+
+
+
+print ("Welcome to the APPLE SAUCE PROGRAM")
+numberOfUserAppleSauces = input ("How many apple sauces do you like: ")
+print("The user said", numberOfUserAppleSauce)
