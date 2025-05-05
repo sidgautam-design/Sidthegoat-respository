a02b0af673fc996e14509fd612edd13e1a59f084
sidgautam-design <siddhartha.gautam889@gmail.com>
2025-05-05 18:06:27 -0400

    Today I worked on working my servo motors to turn the light switch on and off.

--- c++_1/learn/learn.ino
+++ c++_1/learn/learn.ino
@@ -1,14 +1,68 @@
-#include "MyFunctions.h"
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <Servo.h>
 
-const int ledPin = 6;
+// Cloud dashboard variables (linked to switches)
+bool servo1Switch;
+bool servo2Switch;
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
+const char SSID[]     = "YOUR_WIFI_NAME";
+const char PASS[]     = "YOUR_WIFI_PASSWORD";
+
+// Create servo objects
+Servo servo1;  // Connected to pin 3
+Servo servo2;  // Connected to pin 9
+
+void initProperties() {
+  ArduinoCloud.setBoardId(DEVICE_LOGIN_NAME);
+  ArduinoCloud.setSecretDeviceKey(SECRET_DEVICE_KEY);
+
+  ArduinoCloud.addProperty(servo1Switch, READWRITE, ON_CHANGE, onServo1SwitchChange);
+  ArduinoCloud.addProperty(servo2Switch, READWRITE, ON_CHANGE, onServo2SwitchChange);
 }
 
+WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
+
+// Called once at startup
+void setup() {
+  // Serial
+  Serial.begin(9600);
+  delay(1500); 
+
+  // Attach servos
+  servo1.attach(3);
+  servo2.attach(9);
+
+  // Init IoT Cloud
+  initProperties();
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+}
+
+// Called constantly
 void loop() {
-  turnOnLED(ledPin);
-  delay(1000);
-  turnOffLED(ledPin);
-  delay(1000);
+  ArduinoCloud.update();
+}
+
+// When switch 1 is toggled
+void onServo1SwitchChange() {
+  if (servo1Switch) {
+    for (int angle = 0; angle <= 180; angle += 10) {
+      servo1.write(angle);
+      delay(100);
+    }
+  } else {
+    servo1.write(0);  // Reset position
+  }
+}
+
+// When switch 2 is toggled
+void onServo2SwitchChange() {
+  if (servo2Switch) {
+    for (int angle = 0; angle <= 180; angle += 10) {
+      servo2.write(angle);
+      delay(100);
+    }
+  } else {
+    servo2.write(0);  // Reset position
+  }
 }
